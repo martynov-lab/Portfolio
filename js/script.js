083aea5bcdecc2f7a60f4494cb631528135028ac
martynov-lab <arovitm@gmail.com>
2020-09-22 22:00:24 +0300

    My skills
    Created the block "My skills"

--- js/script.js
+++ js/script.js
@@ -14,7 +14,24 @@ document.addEventListener('DOMContentLoaded', () => {
       menu.classList.toggle("active");
       body.classList.toggle("lock");
    });
+   /* ---------------------------------------------- */
+   //плавный скролинг до якоря
+   const anchors = document.querySelectorAll('a[href*="#"]');
 
+   anchors.forEach((anchor) => {
+      anchor.addEventListener('click', function (e) {
+         e.preventDefault();
+
+         const blockID = anchor.getAttribute('href').substr(1)
+
+         document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+         });
+
+         window.scrollBy(0, -50);
+      });
+   });
    /* ---------------------------------------------- */
    //шапка при скроле
    window.addEventListener('scroll', () => {
@@ -27,7 +44,39 @@ document.addEventListener('DOMContentLoaded', () => {
          header.classList.remove("menuSlick");
       }
    });
+   /* ---------------------------------------------- */
+   //portfolio
+   // const link = document.querySelectorAll('.portfolio__nav-link'),
+   //    tab = document.querySelectorAll('.portfolio__item'),
+   //    landingPage = document.querySelectorAll('[data-type = "landingPage"]'),
+   //    storesWebsite = document.querySelectorAll('[data-type = "storesWebsite"]'),
+   //    publicPage = document.querySelectorAll('[data-type = "publicPage"]');
 
+   // landingPage.forEach((e, i) => {
+   //    if ()
+   // });
+
+   // function showTab() {
+
+   // }
+
+
+   // link.forEach(element => {
+   //    element.addEventListener('click', (e) => {
+   //       if (e.classList.contains("landing-page")) {
+   //          e.classList.add("_active");
+   //       }
+   //       tab.forEach(item => {
+   //          if (item.classList.contains())
+   //       });
+   //       landingPage.forEach(item => {
+   //          item.add("_show");
+   //       });
+   //    });
+   // });
+   /* ---------------------------------------------- */
+
+   /* ---------------------------------------------- */
 
    /* ---------------------------------------------- */
 
